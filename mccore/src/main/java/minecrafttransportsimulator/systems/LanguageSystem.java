eaf5797dbc15b0c10bfd4b92a82cd55358f9a784
boot2big <mmangamer14yt@gmail.com>
2026-07-14 01:56:28 -0400

    Fixed random code gank that kept me from compiling

--- mccore/src/main/java/minecrafttransportsimulator/systems/LanguageSystem.java
+++ mccore/src/main/java/minecrafttransportsimulator/systems/LanguageSystem.java
@@ -18,6 +18,7 @@ import minecrafttransportsimulator.jsondefs.JSONItem.JSONBooklet.BookletPage;
 import minecrafttransportsimulator.mcinterface.InterfaceManager;
 import minecrafttransportsimulator.packloading.JSONParser;
 import minecrafttransportsimulator.packloading.PackParser;
+import minecrafttransportsimulator.systems.LanguageSystem.LanguageEntry;
 
 /**
  * System for handling language translation.  This stores all language files found during boot,
@@ -380,6 +381,8 @@ public class LanguageSystem {
     public static final LanguageEntry INTERACT_VEHICLE_DISMOUNTCONFIRM = new LanguageEntry("interact.vehicle.dismountconfirm", "Press Shift again to dismount");
     public static final LanguageEntry INTERACT_ARCADEMODE_ENABLED = new LanguageEntry("interact.arcademode.enabled", "Arcade Mode enabled");
     public static final LanguageEntry INTERACT_ARCADEMODE_DISABLED = new LanguageEntry("interact.arcademode.disabled", "Arcade Mode disabled");
+    public static final LanguageEntry INTERACT_MOUSEYOKE_ENABLED = new LanguageEntry("interact.mouseyoke.enabled", "Mouse Yoke enabled");
+    public static final LanguageEntry INTERACT_MOUSEYOKE_DISABLED = new LanguageEntry("interact.mouseyoke.disabled", "Mouse Yoke disabled");
     
     public static final LanguageEntry INTERACT_PARTREMOVE_HASPARTS = new LanguageEntry("interact.partremove.hasparts", "You must first remove all parts from this part to remove it!");
     public static final LanguageEntry INTERACT_PARTREMOVE_WRENCH = new LanguageEntry("interact.partremove.wrench", "This part requires a wrench to remove!");
